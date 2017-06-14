8146f9d9503b53429b8de8edfbcaa904d2a9b396
Dane Lowe <dane@avidonline.co.nz>
2017-06-14 16:32:35 +1200

    Create jsonlogging.py

---
+++ openupgradelib/jsonlogging.py
@@ -0,0 +1,18 @@
+
+import ROOT.logging as logging
+import sys
+from pythonjsonlogger import jsonlogger
+
+class StackDriverJsonFormatter(jsonlogger.JsonFormatter, object):
+    def __init__(self, fmt="%(levelname) %(message)", style='%', *args, **kwargs):
+        jsonlogger.JsonFormatter.__init__(self, fmt=fmt, *args, **kwargs)
+    def process_log_record(self, log_record):
+        log_record['severity'] = log_record['levelname']
+        del log_record['levelname']
+        return super(StackDriverJsonFormatter, self).process_log_record(log_record)
+
+handler = logging.StreamHandler(sys.stdout)
+formatter = StackDriverJsonFormatter()
+handler.setFormatter(formatter)
+root_logger = logging.getLogger()
+root_logger.addHandler(handler)
